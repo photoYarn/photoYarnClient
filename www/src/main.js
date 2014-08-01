88fc8d6e162b4615364e44162238ffb9e0f79cf9
Kia Fathi <kiafathi@me.com>
2014-08-01 16:42:10 -0700

    Update basic HeaderFooterLayout

--- www/src/main.js
+++ www/src/main.js
@@ -1,42 +1,64 @@
 define(function(require, exports, module) {
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
-    var Surface = require('famous/core/Surface');
+  // import dependencies
+  var Engine = require('famous/core/Engine');
 
-    var mainContext = Engine.createContext();
-    var initialTime = Date.now();
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
 
-    function onDeviceReady() {
-        text.setContent(device.platform);
+  var ImageSurface = require('famous/surfaces/ImageSurface');
+  var Surface = require('famous/core/Surface');
+
+  var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
+  var GridLayout = require('famous/views/GridLayout');
+
+  var mainContext = Engine.createContext();
+  var initialTime = Date.now();
+
+  //Creating Layout
+  var layout = new HeaderFooterLayout({
+    headerSize: 50,
+    footerSize: 50
+  });
+
+
+  //Layout Header
+  layout.header.add(new Surface({
+    content: 'Header',
+    properties: {
+      backgroundColor: 'green',
+      lineHeight: layout.options.headerSize + 'px',
+      textAlign: 'center'
     }
+  }));
 
-    document.addEventListener('deviceready', onDeviceReady, false);
 
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: 'http://code.famo.us/assets/famous_logo.svg',
-        classes: ['double-sided']
-    });
+  //Layout Content
+  var logo = new ImageSurface({
+    size: [200, 200],
+    content: 'http://code.famo.us/assets/famous_logo.svg',
+    classes: ['double-sided']
+  });
 
-    var text = new Surface({
-        size: [200, 200],
-        content: 'none yet'
-    });
+  var centerSpinModifier = new Modifier({
+    origin: [0.5, 0.5],
+    transform: function() {
+        return Transform.rotateY(0.002 * (Date.now() - initialTime));
+    }
+  });
 
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform: function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
+  layout.content.add(centerSpinModifier).add(logo);
 
-    var textModifier = new Modifier({
-        origin: [0.5, 0.5]
-    });
 
-    mainContext.add(textModifier).add(text);
-    mainContext.add(centerSpinModifier).add(logo);
+  //Layout Footer
+  layout.footer.add(new Surface({
+    content: 'Footer',
+    properties: {
+      backgroundColor: 'blue',
+      lineHeight: layout.options.footerSize + 'px',
+      textAlign: 'center'
+    }
+  }));
+
+  mainContext.add(layout);
+
 });
