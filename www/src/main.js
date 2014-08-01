17781353c776416f1cac9a7154094bfffc623d01
Paul Yi <paul.yi326@gmail.com>
2014-08-01 15:23:33 -0700

    able to post images to imgur and get them back and display them

--- www/src/main.js
+++ www/src/main.js
@@ -39,4 +39,46 @@ define(function(require, exports, module) {
 
     mainContext.add(textModifier).add(text);
     mainContext.add(centerSpinModifier).add(logo);
+    // $.ajax({
+    //     type: 'POST',
+    //     url: 'https://api.imgur.com/3/upload',
+    //     headers: {
+    //         Authorization: 'Client-ID ' + 'ef774ae96ae304c',
+    //         // Accept: 'application/json'
+    //     },
+    //     data: {
+    //         image: 'http://explosionhub.com/wp-content/uploads/2012/07/1562-cute-little-cat.jpg',
+    //         title: 'cat'
+    //     },
+    //     success: function(data) {
+    //         console.log(data)
+    //     },
+    //     error: function(error, response) {
+    //         console.log('response', response)
+    //         console.log('error', error)
+    //     },
+    //     // dataType: 'image/jpg'
+    // });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.imgur.com/3/image/La0ANAg',
+        headers: {
+            Authorization: 'Client-ID ' + 'ef774ae96ae304c',
+        },
+        success: function(data) {
+            console.log(data.data.link);
+            var image = new ImageSurface({
+                size: [200, 200],
+            });
+            image.setContent(data.data.link);
+            mainContext.add(image);
+        },
+        error: function(error, response) {
+            console.log('response', response)
+            console.log('error', error)
+        }
+    })
+
+    
 });
