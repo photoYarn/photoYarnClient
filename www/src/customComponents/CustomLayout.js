41a4e3fc41b1df4ab5a3691c30d6e609b256f033
Kia Fathi <kiafathi@me.com>
2014-08-02 17:39:46 -0700

    Refactor layout from main.js into CustomLayout.js

--- www/src/customComponents/CustomLayout.js
+++ www/src/customComponents/CustomLayout.js
@@ -1,120 +1,116 @@
 define(function(require, exports, module) {
   // import dependencies
   'use strict';
-  var Engine = require('famous/core/Engine');
+
   var Modifier = require('famous/core/Modifier');
   var ImageSurface = require('famous/surfaces/ImageSurface');
   var Surface = require('famous/core/Surface');
   var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
   var GridLayout = require('famous/views/GridLayout');
-  var mainContext = Engine.createContext();
   var RenderController = require('famous/views/RenderController');
 
   //custom views
   var NewYarnView = require('views/NewYarnView');
   var FeedView = require('views/FeedView');
   var YarnView = require('views/YarnView');
-
+  
   //custom tools
-  var CustomButton = require('customWidgets/CustomButton');
-  var CustomController = require('customWidgets/CustomController');
+  var CustomButton = require('customComponents/CustomButton');
 
   //Creating Layout
-  var layout = new HeaderFooterLayout({
+  function CustomLayout(){
+    HeaderFooterLayout.apply(this, arguments);
+    _createContent.call(this);
+    _createHeader.call(this);
+    _createFooter.call(this);
+  }
+
+  CustomLayout.prototype = Object.create(HeaderFooterLayout.prototype);
+  CustomLayout.prototype.constructor = CustomLayout;
+  CustomLayout.DEFAULT_OPTIONS = {
+    align: [0.5, 0,5],
     headerSize: 75,
-    footerSize: 50
-  });
-
-  //master controller event handler that keeps track of state
-  
-
-  var masterController = new CustomController();
-  masterController.eventInput.on('FeedClick', function(){
-    console.log('hi Feed');
-    renderController.show(feedView);
-  });
-  masterController.eventInput.on('New YarnClick', function(){
-    console.log('hi New Yarn');
-    renderController.show(newYarnView);
-  });
-  masterController.eventInput.on('YarnClick', function(){
-    console.log('hi Yarn');
-    renderController.show(yarnView);
-  });
-
-
-  //famo.us logo because famo.us is cool!
-  var logo = new ImageSurface({
-    size: [200, 200],
-    content: 'http://img3.wikia.nocookie.net/__cb20130220230859/farmville2/images/a/aa/Super-Fine_Yarn_Ball.png',
-  });
-
-
-  var renderController = new RenderController();
-
-  //Layout Header
-  layout.header.add(new Surface({
-    content: 'Photo Yarn',
-    classes: ['customButton','medgreenBG'],
-    properties: {
-      lineHeight: layout.options.headerSize + 'px',
-      textAlign: 'center'
-    }
-  }));
+    footerSize: 50,
+    origin: [0.5, 0.5],
+    eventTarget: null
+  };
 
   //Layout Content
-  var centerModifier = new Modifier({
-    origin: [0.5, 0.5],
-  });
+  function _createContent(){
+    //famo.us logo because famo.us is cool!
+    var logo = new ImageSurface({
+      size: [200, 200],
+      content: 'http://img3.wikia.nocookie.net/__cb20130220230859/farmville2/images/a/aa/Super-Fine_Yarn_Ball.png',
+    });
 
-  layout.content.add(centerModifier).add(renderController);
-  renderController.show(yarnView);
+    var centerModifier = new Modifier({
+      origin: [0.5, 0.5],
+    });
+    this.feedView = new FeedView({
+      message: 'custom feed view'
+     });
 
-  var feedView = new FeedView({
-    message: 'custom feed view'
-  });
+    this.newYarnView = new NewYarnView({
+      message: 'custom new yarn view'
+    });
 
-  var newYarnView = new NewYarnView({
-    message: 'custom new yarn view'
-  });
+    this.yarnView = new YarnView({
+      message: 'custom yarn view'
+    });
+    
+    this.renderController = new RenderController();
+    this.content.add(centerModifier).add(this.renderController);
+    this.renderController.show(logo);
 
-  var yarnView = new YarnView({
-    message: 'custom yarn view'
-  });
+  }
+  
+  //Layout Header
+  function _createHeader(){
+    this.header.add(new Surface({
+      content: 'Photo Yarn',
+      classes: ['customButton', 'medgreenBG'],
+      properties: {
+        lineHeight: this.options.headerSize + 'px',
+        textAlign: 'center'
+      }
+    }));
+  }
+
 
   //Layout Footer
-  var buttons = [];
+  function _createFooter(){
 
-  buttons.push(new CustomButton({
-    name: 'Feed',
-    classes: ['customButton', 'lightgreenBG'],
-    target: feedView,
-    eventTarget: masterController.eventInput
-  }));
+    this.buttons = [];
 
-  buttons.push(new CustomButton({
-    name: 'New Yarn',
-    classes: ['customButton', 'lightgreenBG'],
-    target: newYarnView,
-    eventTarget: masterController.eventInput
-  }));
+    this.buttons.push(new CustomButton({
+      name: 'Feed',
+      classes: ['customButton', 'lightgreenBG'],
+      eventTarget: this.options.eventTarget
+    }));
 
-  buttons.push(new CustomButton({
-    name: 'Yarn',
-    classes: ['customButton', 'lightgreenBG'],
-    target: yarnView,
-    eventTarget: masterController.eventInput
-  }));
+    this.buttons.push(new CustomButton({
+      name: 'New Yarn',
+      classes: ['customButton', 'lightgreenBG'],
+      eventTarget: this.options.eventTarget
+    }));
 
-  var grid = new GridLayout({
-    dimensions: [3,1]
-  });
-  
-  grid.sequenceFrom(buttons);
+    this.buttons.push(new CustomButton({
+      name: 'Yarn',
+      classes: ['customButton', 'lightgreenBG'],
+      eventTarget: this.options.eventTarget
+    }));
 
-  layout.footer.add(grid);
+    this.buttonGrid = new GridLayout({
+      dimensions: [3,1]
+    });
+    
+    this.buttonGrid.sequenceFrom(this.buttons);
 
-  mainContext.add(layout);
+    this.footer.add(this.buttonGrid);
+  }
+ 
 
-  renderController.show(logo);
+
+
+  module.exports = CustomLayout;
 });
--- www/src/main.js
+++ www/src/main.js
@@ -2,118 +2,35 @@ define(function(require, exports, module) {
   // import dependencies
   'use strict';
   var Engine = require('famous/core/Engine');
-  var Modifier = require('famous/core/Modifier');
-  var ImageSurface = require('famous/surfaces/ImageSurface');
-  var Surface = require('famous/core/Surface');
-  var HeaderFooterLayout = require('famous/views/HeaderFooterLayout');
-  var GridLayout = require('famous/views/GridLayout');
   var mainContext = Engine.createContext();
-  var RenderController = require('famous/views/RenderController');
-
-  //custom views
-  var NewYarnView = require('views/NewYarnView');
-  var FeedView = require('views/FeedView');
-  var YarnView = require('views/YarnView');
 
   //custom tools
-  var CustomButton = require('customComponents/CustomButton');
   var CustomController = require('customComponents/CustomController');
-
-  //Creating Layout
-  var layout = new HeaderFooterLayout({
-    headerSize: 75,
-    footerSize: 50
-  });
-
+  var CustomLayout = require('customComponents/CustomLayout');
 
   //master controller event handler that keeps track of state
   var masterController = new CustomController();
+
+  //Creating Layout
+  var layout = new CustomLayout({
+    headerSize: 75,
+    footerSize: 50,
+    eventTarget: masterController.eventInput
+  });
+
   masterController.eventInput.on('FeedClick', function(){
     console.log('hi Feed');
-    renderController.show(feedView);
+    layout.renderController.show(layout.feedView);
   });
   masterController.eventInput.on('New YarnClick', function(){
     console.log('hi New Yarn');
-    renderController.show(newYarnView);
+    layout.renderController.show(layout.newYarnView);
   });
   masterController.eventInput.on('YarnClick', function(){
     console.log('hi Yarn');
-    renderController.show(yarnView);
+    layout.renderController.show(layout.yarnView);
   });
-
-
-  //famo.us logo because famo.us is cool!
-  var logo = new ImageSurface({
-    size: [200, 200],
-    content: 'http://img3.wikia.nocookie.net/__cb20130220230859/farmville2/images/a/aa/Super-Fine_Yarn_Ball.png',
-  });
-
-
-  var renderController = new RenderController();
-
-  //Layout Header
-  layout.header.add(new Surface({
-    content: 'Photo Yarn',
-    classes: ['customButton','medgreenBG'],
-    properties: {
-      lineHeight: layout.options.headerSize + 'px',
-      textAlign: 'center'
-    }
-  }));
-
-  //Layout Content
-  var centerModifier = new Modifier({
-    origin: [0.5, 0.5],
-  });
-
-  layout.content.add(centerModifier).add(renderController);
-  renderController.show(yarnView);
-
-  var feedView = new FeedView({
-    message: 'custom feed view'
-  });
-
-  var newYarnView = new NewYarnView({
-    message: 'custom new yarn view'
-  });
-
-  var yarnView = new YarnView({
-    message: 'custom yarn view'
-  });
-
-  //Layout Footer
-  var buttons = [];
-
-  buttons.push(new CustomButton({
-    name: 'Feed',
-    classes: ['customButton', 'lightgreenBG'],
-    target: feedView,
-    eventTarget: masterController.eventInput
-  }));
-
-  buttons.push(new CustomButton({
-    name: 'New Yarn',
-    classes: ['customButton', 'lightgreenBG'],
-    target: newYarnView,
-    eventTarget: masterController.eventInput
-  }));
-
-  buttons.push(new CustomButton({
-    name: 'Yarn',
-    classes: ['customButton', 'lightgreenBG'],
-    target: yarnView,
-    eventTarget: masterController.eventInput
-  }));
-
-  var grid = new GridLayout({
-    dimensions: [3,1]
-  });
-  
-  grid.sequenceFrom(buttons);
-
-  layout.footer.add(grid);
 
   mainContext.add(layout);
 
-  renderController.show(logo);
 });
