3970c661a0b715d015d9ab846b8334eaee2c722a
Kyle Craft <craftjk@gmail.com>
2014-08-04 14:24:09 -0700

    feed view displays entries, horizontal scrolling currently buggy

--- www/src/views/FeedEntryView.js
+++ www/src/views/FeedEntryView.js
@@ -3,32 +3,42 @@ define(function(require, exports, module){
   var View = require('famous/core/View');
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
+	var ContainerSurface = require("famous/surfaces/ContainerSurface");
+  var ImageSurface = require('famous/surfaces/ImageSurface');
+  var ScrollContainer = require('famous/views/ScrollContainer');
 	
 	var FeedEntryCaptionView = require('views/FeedEntryCaptionView');
 	var FeedEntryPhotoView = require('views/FeedEntryPhotoView');
 
   function FeedEntryView(){
     View.apply(this, arguments);
-
+		
     _createRootNode.call(this);
-		_createBackground.call(this);
+		// _createBackground.call(this);
     _createCaption.call(this);
     _createPhotos.call(this);
+		_setListeners.call(this);
   }
 
   FeedEntryView.prototype = Object.create(View.prototype);
   FeedEntryView.prototype.constructor = FeedEntryView;
+	
   FeedEntryView.DEFAULT_OPTIONS = {
+		entrySize: [undefined, 190],
 		defaultCaption: 'This is the default caption',
-		captionSize: [undefined, 50],
-		photoSize: [150, 150]
+		captionSize: [undefined, 25],
+		photoSize: [150, 150],
+		properties: {
+			cursor: 'pointer'
+		}
   };
 
   // create root modifier node
   function _createRootNode() {
     this.rootModifier = new Modifier({
-      align: [0.5, 0.5],
-      origin: [0.5, 0.5]
+			size: [this.options.entrySize[0], this.options.entrySize[1]],
+      align: [0, 0],
+      origin: [0, 0]
     });
 
     this.rootNode = this.add(this.rootModifier);
@@ -36,51 +46,67 @@ define(function(require, exports, module){
 	
   function _createBackground() {
     var background = new Surface({
+			size: [this.options.entrySize[0], this.options.entrySize[1]],
       properties: {
-        backgroundColor: '#999'
+        backgroundColor: '#789'
       }
     });
 
-    this.add(background);
+    this.rootNode.add(background);
   };
 	
   function _createCaption() {
-    var caption = new Surface({
+    this.caption = new Surface({
       size: [this.options.captionSize[0], this.options.captionSize[1]],
       content: this.options.defaultCaption,
 			
       properties: {
-				lineHeight: this.options.captionSize[1]
+				lineHeight: this.options.captionSize[1] + 'px',
+				backgroundColor: '#BBB'
       }
     });
 
     var captionModifier = new Modifier({
-      align: [0.5, 0.5],
-      origin: [0.5, 0.5],
+      align: [0, 0],
+      origin: [0, 0]
     });
 
-    this.rootNode.add(captionModifier).add(caption);
+    this.rootNode.add(captionModifier).add(this.caption);
   };
 
   function _createPhotos() {
     this.photos = [];
 
     for (var i = 0; i < 10; i++) {
-      var dummyPhoto = new ImageSurface({
-        size: [this.options.photoSize[0], this.options.photoSize[1]],
-        content: 'http://www.saatchistore.com/217-438-thickbox/pretty-polaroid-notes.jpg',
-        transform: function() {
-            return Transform.rotateY(0.002 * (Date.now() - initialTime));
-        }
-      });
+	    var dummyPhoto = new ImageSurface({
+	      size: [150, 125],
+	      content: 'http://www.saatchistore.com/217-438-thickbox/pretty-polaroid-notes.jpg',
+	      transform: function() {
+	          return Transform.rotateY(0.002 * (Date.now() - initialTime));
+	      }.bind(this)
+	    });
+			
       this.photos.push(dummyPhoto);
+
+			// pipe photo surface to container view
+			dummyPhoto.pipe(this._eventOutput);
     }
+		
+		var photoRowModifier = new Modifier({
+			size: [undefined, this.options.photoSize[1]],
+			align: [0, 1],
+			origin: [0, 1]
+		});
 
     var photoRow = new ScrollContainer();
     photoRow.sequenceFrom(this.photos);
 
-    this.rootNode.add(photoRow);
-  }
-  
+    this.rootNode.add(photoRowModifier).add(photoRow);
+  };
+
+	function _setListeners() {
+		this.caption.pipe(this._eventOutput);
+	}
+
   module.exports = FeedEntryView;
 });
--- www/src/views/FeedView.js
+++ www/src/views/FeedView.js
@@ -3,46 +3,60 @@ define(function(require, exports, module){
   var View = require('famous/core/View');
   var Surface = require('famous/core/Surface');
   var Modifier = require('famous/core/Modifier');
+	var Transform  = require('famous/core/Transform');
   var ScrollContainer = require('famous/views/ScrollContainer');
+  var Scrollview = require("famous/views/Scrollview");
 
   var ImageSurface = require('famous/surfaces/ImageSurface');
 
+	var FeedEntryView = require('views/FeedEntryView');
+
   function FeedView(){
     View.apply(this, arguments);
-
+		
     _createRootNode.call(this);
-		_createBackground.call(this);
     _createFeedEntries.call(this);
   }
 
   FeedView.prototype = Object.create(View.prototype);
   FeedView.prototype.constructor = FeedView;
-  FeedView.DEFAULT_OPTIONS = {
+  
+	FeedView.DEFAULT_OPTIONS = {
     message: 'Default message',
+		entryCount: 4,
+		entryHeight: 175
   };
 
   // create root modifier node
   function _createRootNode() {
     this.rootModifier = new Modifier({
-      align: [0.5, 0.5],
-      origin: [0.5, 0.5],
+      align: [0, 0],
+      origin: [0, 0]
+			// transform: Transform.translate(0,0,-10)
     });
 
     this.rootNode = this.add(this.rootModifier);
   };
-	
-  function _createBackground() {
-    var background = new Surface({
-      properties: {
-        backgroundColor: '#999'
-      }
-    });
-
-    this.add(background);
-  };
 
   function _createFeedEntries() {
+    var feed = new Scrollview();
+		this.entries = [];
+
+    feed.sequenceFrom(this.entries);		
+
+		for (var i = 0; i < this.options.entryCount; i++) {
+			var newEntryView = new FeedEntryView();
+			
+			newEntryView.pipe(feed);
+			this.entries.push(newEntryView);
+		}
 		
+		var feedModifier = new Modifier({
+			size: [undefined, 100],
+			transform: Transform.translate(0, 0, -10)
+		});
+		
+		this.rootNode.add(feedModifier).add(feed);
   };
   
   module.exports = FeedView;
