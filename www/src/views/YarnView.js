748836d476ac94a2ec9e898221a56eb49e14a469
Kia Fathi <kiafathi@me.com>
2014-08-08 19:01:25 -0700

    Implement basic YarnView layout

--- www/src/views/YarnView.js
+++ www/src/views/YarnView.js
@@ -3,27 +3,39 @@ define(function(require, exports, module){
   var View = require('famous/core/View');
   var StateModifier = require('famous/modifiers/StateModifier');
   var ImageSurface = require('famous/surfaces/ImageSurface');
+  var Scrollview = require('famous/views/Scrollview');
 
+  var serverRequests = require('services/serverRequests');
   var catGif = 'http://37.media.tumblr.com/35e8d0682251fa96580100ea6a182e13/tumblr_mst9derOy01re0m3eo1_r12_500.gif';
+  var dummyTarget = "53e5499be71a74d003372cc1";
 
   function YarnView(){
     View.apply(this, arguments);
+    console.log(serverRequests.data);
 
     _createYarn.call(this);
   }
+
+
   YarnView.prototype = Object.create(View.prototype);
   YarnView.prototype.constructor = YarnView;
 
 
 
   function _createYarn(){
-    this.image = new ImageSurface({
-      content: catGif
-    })
+    this.scrollView = new Scrollview({})
 
-    this.imageModifier = new StateModifier({});
+    this.scrollModifier = new StateModifier({});
 
-    this.add(this.imageModifier).add(this.image);
+    this.add(this.scrollModifier).add(this.scrollView);
+    this.scrollView.on('click', function(){
+      this.magicTime();
+    }.bind(this));
+  }
+
+  YarnView.prototype.magicTime = function(){
+    var targetYarn = serverRequests.data[serverRequests.cache[dummyTarget]];
+    console.log(targetYarn);
   }
 
 
