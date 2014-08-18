f0769afedc08fca21023a401b52a5df5bc8b46ea
Kia Fathi <kiafathi@me.com>
2014-08-17 17:33:45 -0700

    [FIX] Update the way the yarn view animates

--- app/src/main.js
+++ app/src/main.js
@@ -19,21 +19,21 @@ var oauth = require('./customComponents/oauth');
 Uncomment this eventListener and disable the serverRequests.getData call below for on device use
 Logs in w/ oauth through facebook
 */
-document.addEventListener('deviceready', function() {
-  console.log('device ready!');
-  var runningInCordova = true;
-  oauth.login(function(response) {
-    if (response.status === 'connected') {
-        console.log('fb login success, received access token');
-        // check against database to see if new user
-        // or current user by sending request to
-        serverRequests.loginToFacebook(response);
-    } else {
-        console.log('login failed', response.error);
-        serverRequests.getData();
-    }
-  });      
-}, false);
+// document.addEventListener('deviceready', function() {
+//   console.log('device ready!');
+//   var runningInCordova = true;
+//   oauth.login(function(response) {
+//     if (response.status === 'connected') {
+//         console.log('fb login success, received access token');
+//         // check against database to see if new user
+//         // or current user by sending request to
+//         serverRequests.loginToFacebook(response);
+//     } else {
+//         console.log('login failed', response.error);
+//         serverRequests.getData();
+//     }
+//   });      
+// }, false);
 
 // create display context
 var mainContext = Engine.createContext();
@@ -51,4 +51,4 @@ var layout = new CustomLayout({
 mainContext.add(layout);
 
 // Uncomment below get method to enable on computer testing
-// serverRequests.getData();
+serverRequests.getData();
