d2ae39b339312957014102309b4f3089c452ef73
Kia Fathi <kiafathi@me.com>
2014-08-18 11:48:21 -0700

    [REFACTOR] Update initial login state

--- app/src/main.js
+++ app/src/main.js
@@ -19,21 +19,30 @@ var oauth = require('./customComponents/oauth');
 Uncomment this eventListener and disable the serverRequests.getData call below for on device use
 Logs in w/ oauth through facebook
 */
-// document.addEventListener('deviceready', function() {
-//   console.log('device ready!');
-//   var runningInCordova = true;
-//   oauth.login(function(response) {
-//     if (response.status === 'connected') {
-//         console.log('fb login success, received access token');
-//         // check against database to see if new user
-//         // or current user by sending request to
-//         serverRequests.loginToFacebook(response);
-//     } else {
-//         console.log('login failed', response.error);
-//         serverRequests.getData();
-//     }
-//   });      
-// }, false);
+document.addEventListener('deviceready', function() {
+  console.log('device ready!');
+  var runningInCordova = true;
+  console.log('serverToken', window.localStorage.getItem('serverToken'));
+  console.log('facebookName', window.localStorage.getItem('facebookName'));
+  console.log('facebookId', window.localStorage.getItem('facebookId'));
+  if(!window.localStorage.getItem('serverToken')){
+    oauth.login(function(response) {
+      if (response.status === 'connected') {
+        console.log('fb login success, received access token');
+        // check against database to see if new user
+        // or current user by sending request to
+        serverRequests.loginToFacebook(response);
+      } else {
+        console.log('login failed', response.error);
+        serverRequests.getData();
+      }
+    });          
+  } else {
+    serverRequests.user.id = window.localStorage.getItem('facebookId');
+    serverRequests.user.name = window.localStorage.getItem('facebookName');
+    serverRequests.getData();
+  }
+}, false);
 
 // create display context
 var mainContext = Engine.createContext();
@@ -51,4 +60,4 @@ var layout = new CustomLayout({
 mainContext.add(layout);
 
 // Uncomment below get method to enable on computer testing
-serverRequests.getData();
+// serverRequests.getData();
